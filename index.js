3d7c2092d1965f22bb42b19e1472142faaa2af7f
John Ratanaprasatporn <jratana1@gmail.com>
2020-08-27 03:07:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var empty = [];
+  for (let i = 0; i<musicians.length; i++ ){
+    empty[i] = musicians[i]+' plays '+instruments[i]
+  }
+  return(empty)
+}
